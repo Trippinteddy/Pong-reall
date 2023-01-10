7706e4765fe026ea94cfe8d1ebf8a5226f2e9af4
Trippinteddy <83976342+Trippinteddy@users.noreply.github.com>
2023-01-10 11:09:01 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,8 +43,8 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("Adventure_Meme.mp3"); //load sound
-soundArray.push(wallSound); //add sound to list of sounds
+//export var wallSound = new soundFile("Adventure_Meme.mp3"); //load sound
+//soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("tap-resonant.mp3");
 soundArray.push(paddleSound);
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("Adventure_Meme.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = 0; //turn down volume
 
 export var adventureMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
 soundArray.push(adventureMusic);
